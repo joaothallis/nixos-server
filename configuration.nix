fb0f83f5b9a98e59253e4a5fec265843ca6a03ba
Claudio Neto <claudionts@gmail.com>
2024-12-22 20:51:08 -0300

    add ssh access (#1)

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,7 @@
   services.openssh.enable = true;
   users.users.root.openssh.authorizedKeys.keys = [
     ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIywXCJvuYxPSDXk/tNY+pTCyJW+bXbZtJ3YQH/jDNHp joao@Joaos-MacBook-Pro.local''
+    ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWkDdEXgI2cMb8LQIQFDpvrCUvNxKkTaL2h4FsOESdX jose.csilva@BRRIOMN045510.local''
   ];
   system.stateVersion = "23.11";
 }
