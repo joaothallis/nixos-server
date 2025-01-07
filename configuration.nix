aa753470af37056be15255cbe149a15b28389308
Claudio Neto <claudionts@gmail.com>
2025-01-06 21:48:50 -0300

    change ssh key

--- configuration.nix
+++ configuration.nix
@@ -30,7 +30,7 @@
   users.users = {
     root.openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIywXCJvuYxPSDXk/tNY+pTCyJW+bXbZtJ3YQH/jDNHp joao@Joaos-MacBook-Pro.local''
-      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWkDdEXgI2cMb8LQIQFDpvrCUvNxKkTaL2h4FsOESdX jose.csilva@BRRIOMN045510.local''
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/zA4tBQIJTlEyqlrBtHL6G92BSyF7igftzb3zk1zvG claudionts@gmail.com''
     ];
 
     github = {
